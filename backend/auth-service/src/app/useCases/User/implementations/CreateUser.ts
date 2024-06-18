2b9172046bf5665f236624d5d1f9919a4d9dc632
aswanth <aswanthndl@gmail.com>
2024-06-18 23:52:20 +0530

    feat(auth-service): add Username for registration
    Add username when register with userdetails for the auth service. This includes:
    - Including username from the request and saving to the database.
    - Added 2 more fields Favourites and Subscribed

--- backend/auth-service/src/app/useCases/User/implementations/CreateUser.ts
+++ backend/auth-service/src/app/useCases/User/implementations/CreateUser.ts
@@ -11,9 +11,13 @@ export default class CreateUser {
     this.passwordHasher = passwordHasher;
   }
 
-  public async execute(email: string, password: string): Promise<IUser> {
+  public async execute(email: string, password: string, username: string): Promise<IUser> {
     const hashedPassword = await this.passwordHasher.hash(password);
-    const user = await this.userRepository.create({ email, password: hashedPassword } as IUser);
-    return user;
+    const user = await this.userRepository.findByEmail(email);
+    if (user) {
+      throw new Error('User already exist');
+    }
+    const newUser = await this.userRepository.create({ email, password: hashedPassword, username } as IUser);
+    return newUser;
   }
 }
