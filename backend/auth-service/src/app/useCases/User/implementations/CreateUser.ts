f81b11975670edf7545a83f14441b1c344697703
aswanth <aswanthndl@gmail.com>
2024-06-19 11:07:14 +0530

    feat(auth-service): add rabbitmq publisher
    Implement rabbitmq messaging system for the auth service. This includes:
    - Publishing a message when a user registers an account.
    - This message must be consumed by the roadmap-service to store the user details in the profile database.

--- backend/auth-service/src/app/useCases/User/implementations/CreateUser.ts
+++ backend/auth-service/src/app/useCases/User/implementations/CreateUser.ts
@@ -1,5 +1,6 @@
 import UserRepository from '../../../repositories/UserRepository';
 import PasswordHasher from '../../../providers/PasswordHasher';
+import Publisher from '../../../../infra/messaging/rabbitmq/Publisher';
 import { IUser } from '../../../../infra/databases/mongoose/models/User';
 
 export default class CreateUser {
@@ -15,9 +16,14 @@ export default class CreateUser {
     const hashedPassword = await this.passwordHasher.hash(password);
     const user = await this.userRepository.findByEmail(email);
     if (user) {
-      throw new Error('User already exist');
+      throw new Error('User already exists');
     }
     const newUser = await this.userRepository.create({ email, password: hashedPassword, username } as IUser);
+
+    // Publish user details to RabbitMQ
+    const publisher = await Publisher.getInstance();
+    await publisher.publish('user.registration', JSON.stringify({ email, username }));
+
     return newUser;
   }
 }
