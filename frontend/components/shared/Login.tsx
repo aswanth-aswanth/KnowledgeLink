f29ffcbdfaa8cc3dca2275754f6903d8f50a0fb4
aswanth <aswanthndl@gmail.com>
2024-06-25 18:13:42 +0530

    feat(frontend): Complete Login design and api integration

--- frontend/components/shared/Login.tsx
+++ frontend/components/shared/Login.tsx
@@ -1,31 +1,71 @@
 "use client";
+
 import React, { useState } from "react";
 import Link from "next/link";
+import { useSearchParams, useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-  const handleSubmit = (e) => {
+  const error = searchParams.get("error");
+  const success = searchParams.get("success");
+
+  if (error) {
+    toast.error("Google authentication was unsuccessful. Please try again.");
+  } else if (success) {
+    console.log("Success");
+    router.push("/");
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Login submitted", { email, password, username });
+    setIsLoading(true);
+    console.log("Login submitted", { email, password });
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`,
+        {
+          email,
+          password,
+        }
+      );
+      console.log("Response : ", response);
+
+      toast("Login successful!", {
+        icon: "👏",
+      });
+      router.push("/");
+    } catch (error) {
+      if (error instanceof Error && error.response && error.response.data) {
+        const axiosError = error as AxiosError;
+        console.log("Error : ", axiosError.response.data.error);
+        const errorMsg = axiosError.response.data.error;
+        toast.error(errorMsg);
+      } else {
+        console.error("Unexpected error:", error);
+        toast.error("An unexpected error occurred");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleGoogleAuth = () => {
+    const authServiceUrl = process.env.NEXT_PUBLIC_AUTH_SERVICE_URL;
+    window.location.href = `${authServiceUrl}/google`;
   };
 
   const socialButtons = [
     {
-      name: "GitHub",
-      icon: (
-        <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-          <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z" />
-        </svg>
-      ),
-      color: "text-gray-800 hover:bg-gray-800 hover:text-white",
-    },
-    {
       name: "Google",
       icon: (
-        <svg viewBox="0 0 24 24" width="20" height="20">
+        <svg viewBox="0 0 24 24" width="35" height="35" fill="currentColor">
           <path
             fill="#4285F4"
             d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
@@ -45,34 +85,50 @@ const Login = () => {
           <path fill="none" d="M1 1h22v22H1z" />
         </svg>
       ),
-      color: "hover:bg-blue-50",
+      color: "bg-gray-200 hover:bg-gray-100",
+      onClick: () => {
+        handleGoogleAuth();
+      },
+    },
+    {
+      name: "GitHub",
+      icon: (
+        <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
+          <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z" />
+        </svg>
+      ),
+      color: "bg-gray-800 text-white hover:bg-gray-900",
+      onClick: () => {},
     },
     {
       name: "Facebook",
       icon: (
-        <svg viewBox="0 0 24 24" width="20" height="20" fill="#1877F2">
+        <svg viewBox="0 0 24 24" width="35" height="35" fill="#1877F2">
           <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
         </svg>
       ),
-      color: "hover:bg-blue-50",
+      color: "bg-gray-200 hover:bg-gray-100",
+      onClick: () => {
+        // Implement Facebook authentication here
+      },
     },
   ];
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-400 via-pink-500 to-red-500 flex items-center justify-center sm:p-4 p-2">
-      <div className="w-full max-w-md bg-white md:p-8 p-4  rounded-lg shadow-lg transform transition-all">
+      <div className="w-full max-w-md bg-white md:p-8 p-4 rounded-lg shadow-lg transform transition-all">
         <h2 className="text-2xl font-bold text-center text-gray-900 mb-4">
           Welcome back
         </h2>
 
-        <div className="flex flex-col space-y-4 mb-4">
-          {socialButtons.map(({ name, icon, color }) => (
+        <div className="flex justify-center space-x-4 mb-4">
+          {socialButtons.map(({ name, icon, color, onClick }) => (
             <button
               key={name}
-              className={`flex items-center justify-center px-3 py-2 border border-gray-300 rounded-md text-sm font-medium bg-white ${color}`}
+              className={`flex items-center justify-center w-12 h-12 rounded-full ${color}`}
+              onClick={onClick}
             >
-              <span className="mr-1">{icon}</span>
-              Sign in with {name}
+              {icon}
             </button>
           ))}
         </div>
@@ -83,28 +139,12 @@ const Login = () => {
           </div>
           <div className="relative flex justify-center text-sm">
             <span className="px-2 bg-white text-gray-500">
-              Or continue with
+              Or continue with email
             </span>
           </div>
         </div>
 
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Username
-            </label>
-            <input
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              required
-            />
-          </div>
           <div>
             <label
               htmlFor="email"
@@ -139,12 +179,20 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="w-full bg-indigo-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed"
+            disabled={isLoading}
           >
-            Sign in
+            {isLoading ? (
+              <div className="flex items-center justify-center">
+                <div className="w-5 h-5 border-t-2 border-b-2 border-white rounded-full animate-spin"></div>
+                <span className="ml-2">Signing in...</span>
+              </div>
+            ) : (
+              "Sign in"
+            )}
           </button>
           <p className="text-center text-sm">
-            Don't have an account ?.
+            Don't have an account?{" "}
             <Link href={"sign-up"} className="text-blue-500">
               Register here.
             </Link>
