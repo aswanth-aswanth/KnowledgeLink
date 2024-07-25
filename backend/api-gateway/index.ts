265828cf78c49876b81bcca5c3c13fec284d5907
aswanth <aswanthndl@gmail.com>
2024-07-26 00:33:16 +0530

    feat(api-gateway): Add recommendation-service to api-gateway

--- backend/api-gateway/index.ts
+++ backend/api-gateway/index.ts
@@ -23,6 +23,7 @@ const services: { [key: string]: string } = {
   post: `${process.env.BASE_URL}:5003`,
   notification: `${process.env.BASE_URL}:5004`,
   chat: `${process.env.BASE_URL}:5005`,
+  recommendation: `${process.env.BASE_URL}:5006`,
 };
 
 const authProxy: RequestHandler = createProxyMiddleware({
@@ -55,12 +56,18 @@ const chatProxy: RequestHandler = createProxyMiddleware({
   changeOrigin: true,
 });
 
+const recommendationProxy: RequestHandler = createProxyMiddleware({
+  target: services.recommendation,
+  changeOrigin: true,
+});
+
 app.use('/auth', authProxy);
 app.use('/profile', profileProxy);
 app.use('/roadmap', roadmapProxy);
 app.use('/post', postProxy);
 app.use('/notification', notificationProxy);
 app.use('/chat', chatProxy);
+app.use('/recommendation', recommendationProxy);
 
 const PORT: number = 4000;
 app.listen(PORT, () => {
