66eae37ff170db94959311b5b68bb03c2f09342c
aswanth <aswanthndl@gmail.com>
2024-08-18 17:07:05 +0530

    fix(frontend): Fix frontend api fetching url for getting roadmaps.

--- frontend/components/roadmap/Roadmaps.tsx
+++ frontend/components/roadmap/Roadmaps.tsx
@@ -15,7 +15,7 @@ export default function Roadmaps() {
   const getRoadmapByType = async (roadmapType: string) => {
     setLoading(true);
     try {
-      const res = await apiClient.get(`/roadmap/type?type=${roadmapType}`);
+      const res = await apiClient(`/roadmap/type?type=${roadmapType}`);
       setRoadmapData(res.data);
     } catch (error) {
       console.log('Error: ', error);
