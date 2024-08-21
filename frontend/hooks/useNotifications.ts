3bf1340a424a71d5ac41a06c55c997ce3d899390
aswanth <aswanthndl@gmail.com>
2024-08-21 07:04:37 +0530

    fix(frontend): Adjust urls to fix socket.io connection

--- frontend/hooks/useNotifications.ts
+++ frontend/hooks/useNotifications.ts
@@ -5,30 +5,30 @@ import toast from 'react-hot-toast';
 export function useNotifications(url: string, userEmail: string | null) {
   const token = localStorage.getItem('token');
   useEffect(() => {
-    const socket = io(url, {
-      auth: { token },
-      withCredentials: true,
-    });
+    // const socket = io(url, {
+    //   auth: { token },
+    //   withCredentials: true,
+    // });
 
     if (userEmail) {
-      socket.emit('register', userEmail);
+      // socket.emit('register', userEmail);
     }
 
-    socket.on('notification', (notification: { type: string; message: string; postId: string }) => {
-      console.count("toast notified2");
-      toast(notification.message, {
-        icon: '👏',
-        duration: 6000,
-        style: {
-          borderRadius: '10px',
-          background: '#333',
-          color: '#fff',
-        },
-      });
-    });
+    // socket.on('notification', (notification: { type: string; message: string; postId: string }) => {
+    //   console.count("toast notified2");
+    //   toast(notification.message, {
+    //     icon: '👏',
+    //     duration: 6000,
+    //     style: {
+    //       borderRadius: '10px',
+    //       background: '#333',
+    //       color: '#fff',
+    //     },
+    //   });
+    // });
 
     return () => {
-      socket.disconnect();
+      // socket.disconnect();
     };
   }, [url, userEmail]);
 }
