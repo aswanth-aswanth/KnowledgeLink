3e46bd4338c49d55679d01b9c99e3d13d70bec3b
aswanth <aswanthndl@gmail.com>
2024-08-23 23:13:49 +0530

    feat(backend): Add loggin and error handler

--- backend/chat-service/src/presentation/express/settings/app.ts
+++ backend/chat-service/src/presentation/express/settings/app.ts
@@ -1,10 +1,25 @@
 import express from "express";
 import chatRouter from "../routers/chat";
+import { errorHandler } from '../../../infra/http/middleware/errorHandler';
+import logger from '../../../infra/logging/logger';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    logger.info({
+        method: req.method,
+        path: req.path,
+        body: req.body,
+        query: req.query,
+    });
+    next();
+});
+
 app.use('/', chatRouter);
+
+app.use(errorHandler);
 
 export default app;
--- backend/post-service/src/presentation/express/settings/app.ts
+++ backend/post-service/src/presentation/express/settings/app.ts
@@ -1,12 +1,27 @@
 import express from "express";
 import postRouter from "../routers/post";
 import shortRouter from "../routers/short";
+import { errorHandler } from '../../../infra/http/middleware/errorHandler';
+import logger from '../../../infra/logging/logger';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    logger.info({
+        method: req.method,
+        path: req.path,
+        body: req.body,
+        query: req.query,
+    });
+    next();
+});
+
 app.use('/', postRouter);
 app.use('/short', shortRouter);
+
+app.use(errorHandler);
 
 export default app;
