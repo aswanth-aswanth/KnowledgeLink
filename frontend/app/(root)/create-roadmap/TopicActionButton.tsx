2f2a39a661747f1a66b92f767c18564baad3099f
aswanth-aswanth <aswanthndl@gmail.com>
2025-04-23 10:32:06 +0530

    fix: fix TopicActionButton type

--- frontend/app/(root)/create-roadmap/TopicActionButton.tsx
+++ frontend/app/(root)/create-roadmap/TopicActionButton.tsx
@@ -1,4 +1,19 @@
-const TopicActionButton = ({ onClick, icon: Icon, title, className = '' }) => (
+import { FC } from 'react';
+import { LucideIcon } from 'lucide-react';
+
+interface TopicActionButtonProps {
+  onClick: () => void;
+  icon: LucideIcon;
+  title: string;
+  className?: string;
+}
+
+const TopicActionButton: FC<TopicActionButtonProps> = ({
+  onClick,
+  icon: Icon,
+  title,
+  className = '',
+}) => (
   <button
     onClick={onClick}
     className={`p-1 rounded-md focus:outline-none text-gray-400 bg-gray-100 hover:bg-gray-200 dark:text-gray-500 dark:bg-gray-800 dark:hover:bg-gray-700 transition-opacity duration-200 ${className}`}
